fcd0c89d54665b4728ae08a9b1bfcd229ca68fd7
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-08-31 04:31:20 +0530

    currecny convertor with dark mode

---
+++ Currency-Convertor/src/context/theme.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [themeMode, setThemeMode] = useState('light');
+
+  const toggleTheme = () => {
+    setThemeMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ themeMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}; 
--- Currency-Convertor/src/main.jsx
+++ Currency-Convertor/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider } from './context/theme.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <ThemeProvider>
   <StrictMode>
     <App />
   </StrictMode>,
+  </ThemeProvider>
 )
